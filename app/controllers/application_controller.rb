c0d6e22ae729e120e1168f48b172c05d0d7b5a5f
TJ Koblentz <tj.koblentz@gmail.com>
2010-08-25 00:43:01 -0700

    Tracking IP address for paper_trail whodunnit. Hide first two chunks for anonymity? Display time and who for last edit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+  
+  protected
+  
+  def current_user
+    request.env['REMOTE_ADDR'].gsub!(/([0-9]+).([0-9]+).([0-9]+).([0-9]+)/, 'xxx.xxx.\3.\4')
+  end
 end
